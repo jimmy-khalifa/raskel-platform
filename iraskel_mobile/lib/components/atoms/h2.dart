75d285b70842757e9361a78a7a60d73890fab255
sihemsioui <sihem.sioui@gmail.com>
2021-12-30 14:08:04 +0100

    get started modif

---
+++ iraskel_mobile/lib/components/atoms/h2.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class H2Text extends StatelessWidget {
+  final h2text;
+  final align;
+  const H2Text({Key? key, this.h2text, this.align}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      heightFactor: 2,
+      child: Text(
+        h2text,
+        style: GoogleFonts.tajawal(
+          textStyle: const TextStyle(
+            color: Color(0xFF65C88D),
+            fontWeight: FontWeight.w700,
+            fontSize: 34.0,
+            decoration: TextDecoration.none,
+          ),
+        ),
+        textAlign: TextAlign.start,
+      ),
+    );
+  }
+}
---
+++ iraskel_mobile/lib/components/organisms/getstartedtemp.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:iraskel_mobile/components/atoms/corousel.dart' as global;
+import 'package:iraskel_mobile/components/organisms/stepperbar.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final CarouselController controller = CarouselController();
+
+class GStartTemplate extends StatefulWidget {
+  final orientation, pages;
+
+  const GStartTemplate({Key? key, this.orientation, this.pages})
+      : super(key: key);
+
+  @override
+  GStartTemplateState createState() => GStartTemplateState();
+}
+
+class GStartTemplateState extends State<GStartTemplate> {
+  int selectedIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  setSelectedIndex(value) {
+    setState(() {
+      selectedIndex = value;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        margin: widget.orientation == Orientation.portrait
+            ? EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 20,
+                left: MediaQuery.of(context).size.width / 25,
+                right: MediaQuery.of(context).size.width / 25,
+                bottom: MediaQuery.of(context).size.height / 45)
+            : EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 20,
+                left: MediaQuery.of(context).size.width / 2.5,
+                right: MediaQuery.of(context).size.width / 2.5,
+                bottom: MediaQuery.of(context).size.height / 45),
+        elevation: 0,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(children: [
+          global.AppCarouselSlider(
+            orientation: widget.orientation,
+            setter: setSelectedIndex,
+            controller: controller,
+          ),
+          StepperBar(
+            buttonText: 'passer',
+            horizPadding: 15,
+            pagesLength: widget.pages.length,
+            controller: controller,
+            selectedIndex: selectedIndex,
+            setter: setSelectedIndex,
+          ),
+        ]));
+  }
+}
