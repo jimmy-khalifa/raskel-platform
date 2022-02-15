b093a077746529c7d86b964650e962f1b40eafa6
rimhorchani <rima62630@gmail.com>
2022-02-15 14:15:05 +0100

    add cin

--- platform/raskel/producer/resolvers/__init__.py
+++ platform/raskel/producer/resolvers/__init__.py
@@ -1,2 +1,2 @@
-from .producer import prod_mutation, prod_query, upload_mutation
+from .producer import prod_mutation, prod_query, upload_mutation,upload_mutation_cin_front,upload_mutation_cin_back
 from .property import property_query, property_mutation
--- platform/raskel/raskel/schema.py
+++ platform/raskel/raskel/schema.py
@@ -6,7 +6,7 @@ from security.resolvers import user_query, user_mutation
 from geo.schema import country_type_defs, municipality_type_defs, district_type_defs, state_type_defs, address_type_defs
 from geo.resolvers import country_query, state_query, district_query, municipality_query, address_query, address_mutation
 from producer.schema import prod_type_defs, property_type_defs
-from producer.resolvers import prod_query, prod_mutation, property_query, property_mutation, upload_mutation
+from producer.resolvers import prod_query, prod_mutation, property_query, property_mutation, upload_mutation, upload_mutation_cin_front,upload_mutation_cin_back
 from company.schema import company_type_defs
 from company.resolvers import company_mutation, company_query
 from ariadne import gql
@@ -35,6 +35,8 @@ type Query{
 
 type Mutation{
     uploadUserImage(image: Upload!): Boolean!
+    uploadCinFront(cin_pic_front: Upload!): Boolean!
+    uploadCinBack(cin_pic_back: Upload!): Boolean!
     create_user(input: UserInput): UserResults
     verify_phone_number(input: CodeInput): CodeResult
     resend_verification_code(input: String): String
@@ -60,5 +62,5 @@ jwt_mutation.set_field('tokenAuth', resolve_token_auth)
 schema = make_executable_schema([user_type_defs, jwt_schema, country_type_defs, state_type_defs, 
 municipality_type_defs, district_type_defs, address_type_defs, prod_type_defs, property_type_defs, company_type_defs, type_defs], 
 [user_query, user_mutation, jwt_mutation, country_query, state_query, municipality_query, company_query, company_mutation,
-district_query, address_query, address_mutation, prod_query, prod_mutation, property_query, property_mutation,upload_mutation], 
+district_query, address_query, address_mutation, prod_query, prod_mutation, property_query, property_mutation,upload_mutation,upload_mutation_cin_front,upload_mutation_cin_back], 
 [GenericScalar,upload_scalar])
